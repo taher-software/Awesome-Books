42187451e76b203b2408f3b38ff0bed4696aeec7
Fondem Junior <fondemjunior@yahoo.com>
2021-10-18 22:09:33 +0100

    Add some scripts

--- scripts/objects.js
+++ scripts/objects.js
@@ -2,6 +2,7 @@ console.log("I test");
 /* Declarate variables. */
 const booksDiv = document.querySelector('.books');
 const addBtn = document.querySelector('.add-book');
+const remove = document.querySelector('.Remove');
 let books = [];
 /* utilities */
 function newBook(title,author){
@@ -16,13 +17,23 @@ function updateBooks(){
   const title = document.getElementById('title-book').value;
   const author = document.getElementById('author-book').value;
   newBook(title,author);
-  booksDiv.innerHTML += `<div><p> ${title} by ${author} </p> <button>Remove</button></div>`;
+  booksDiv.innerHTML += `<div><p> ${title} by ${author} </p> <input type= 'button' class='Remove' value ='Remove'></div>`;
 
+}
+function removeBooks(){
+  const title = document.getElementById('title-book').value;
+  remove(title);
+  booksDiv.innerHTML += `<div><p> ${title} by ${author} </p> <input type= 'button' class='Remove' value ='Remove'></div>`;
 }
 /*Loop into Books array */
 booksDiv.innerHTML = '';
 books.forEach((elem) => {
-  booksDiv.innerHTML += `<div> <p> ${elem.title} by ${elem.author}</p> <button>Remove</button></div> `;
+  booksDiv.innerHTML += `<div> <p> ${elem.title} by ${elem.author}</p> <input type= 'button' class= 'Remove' value='Remove'></div> `;
 });
 /* tracking events */
 addBtn.addEventListener('click',updateBooks);
+
+// remove button
+
+
+remove.addEventListener('click', removeBooks);
