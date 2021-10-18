0efd70ca3133d1f3872fd8b70c652eb80f744491
Taher <ttaherhagui@gmail.com>
2021-10-19 00:50:35 +0100

    Add local storage feature.

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,39 +1,65 @@
-console.log("I test");
 /* Declarate variables. */
 const booksDiv = document.querySelector('.books');
 const addBtn = document.querySelector('.add-book');
-const remove = document.querySelector('.Remove');
+let removeBook = Array.from(document.querySelectorAll('.Remove'));
 let books = [];
 /* utilities */
 function newBook(title,author){
   books.push({title: title, author: author});
 }
-function remove(title){
-  books.filter((elem) => {
-      elem.title != title;
-  });
+function remove(tit){
+  ind = books.indexOf(tit);
+  books.splice(ind,1);
+  
 }
+function removeBooks(e){
+    const targetELement = e.target;
+    const parentElement = targetELement.parentNode;
+    const data = Array.from(parentElement.children) [0].textContent;
+    const tl = data.split('by')[0];
+    remove(tl);
+    parentElement.innerHTML = '';
+  }
 function updateBooks(){
   const title = document.getElementById('title-book').value;
   const author = document.getElementById('author-book').value;
+  const newBlock = `<div><p> ${title} by ${author} </p> <input type= 'button' class='Remove' value ='Remove'><hr></div>`
   newBook(title,author);
-  booksDiv.innerHTML += `<div><p> ${title} by ${author} </p> <input type= 'button' class='Remove' value ='Remove'></div>`;
+  booksDiv.innerHTML += newBlock;
+  removeBook = Array.from(document.querySelectorAll('.Remove'));
+  removeBook.forEach((elem) => {
+    elem.addEventListener('click', removeBooks, false);
+});
 
 }
-function removeBooks(){
-  const title = document.getElementById('title-book').value;
-  remove(title);
-  booksDiv.innerHTML += `<div><p> ${title} by ${author} </p> <input type= 'button' class='Remove' value ='Remove'></div>`;
-}
+
 /*Loop into Books array */
 booksDiv.innerHTML = '';
 books.forEach((elem) => {
-  booksDiv.innerHTML += `<div> <p> ${elem.title} by ${elem.author}</p> <input type= 'button' class= 'Remove' value='Remove'></div> `;
+  booksDiv.innerHTML += `<div> <p> ${elem.title} by ${elem.author}</p> <input type= 'button' class= 'Remove' value='Remove' <hr>></div> `;
 });
 /* tracking events */
 addBtn.addEventListener('click',updateBooks);
-
-// remove button
-
-
-remove.addEventListener('click', removeBooks);
+/* store datas. */
+const titleBook = document.getElementById('title-book');
+const authorBook = document.getElementById('author-book');
+function setStorage(){
+  const inputBook = {
+    bookTitle: document.getElementById('title-book').value,
+    bookAuthor: document.getElementById('author-book').value
+  };
+  localStorage.setItem('BookIdentifier', JSON.stringify(inputBook));
+}
+function setDesign() {
+  const book = JSON.parse(localStorage.getItem('BookIdentifier'));
+  document.getElementById('title-book').value = book.bookTitle;
+  document.getElementById('author-book').value = book.bookAuthor;
+}
+if(!localStorage.getItem('BookIdentifier')){
+  setStorage();
+}
+else {
+  setDesign();
+}
+titleBook.onchange = setStorage;
+authorBook.onchange = setStorage;
