e2fa3b9ece6aac49794c25bbf2ab0131ad2a1a14
Taher <ttaherhagui@gmail.com>
2021-10-19 12:40:00 +0100

    fix linter errors.

--- scripts/objects.js
+++ scripts/objects.js
@@ -2,51 +2,55 @@
 const booksDiv = document.querySelector('.books');
 const addBtn = document.querySelector('.add-book');
 let removeBook = Array.from(document.querySelectorAll('.Remove'));
-let books = [];
+const books = [];
 /* utilities */
-function newBook(title,author){
-  books.push({title: title, author: author});
+function newBook(tit, auth) {
+  const title = tit;
+  const author = auth;
+  const myBook = {
+    title,
+    author,
+  };
+  books.push(myBook);
 }
-function remove(tit){
-  ind = books.indexOf(tit);
-  books.splice(ind,1);
-  
+function remove(tit) {
+  const ind = books.indexOf(tit);
+  books.splice(ind, 1);
 }
-function removeBooks(e){
-    const targetELement = e.target;
-    const parentElement = targetELement.parentNode;
-    const data = Array.from(parentElement.children) [0].textContent;
-    const tl = data.split('by')[0];
-    remove(tl);
-    parentElement.innerHTML = '';
-  }
-function updateBooks(){
+function removeBooks(e) {
+  const targetELement = e.target;
+  const parentElement = targetELement.parentNode;
+  const data = Array.from(parentElement.children)[0].textContent;
+  const tl = data.split('by')[0];
+  remove(tl);
+  parentElement.innerHTML = '';
+}
+function updateBooks() {
   const title = document.getElementById('title-book').value;
   const author = document.getElementById('author-book').value;
-  const newBlock = `<div><p> ${title} by ${author} </p> <input type= 'button' class='Remove' value ='Remove'><hr></div>`
-  newBook(title,author);
+  const newBlock = `<div><p> ${title} by ${author} </p> <input type= 'button' class='Remove' value ='Remove'><hr></div>`;
+  newBook(title, author);
   booksDiv.innerHTML += newBlock;
   removeBook = Array.from(document.querySelectorAll('.Remove'));
   removeBook.forEach((elem) => {
     elem.addEventListener('click', removeBooks, false);
-});
-
+  });
 }
 
-/*Loop into Books array */
+/* Loop into Books array */
 booksDiv.innerHTML = '';
 books.forEach((elem) => {
   booksDiv.innerHTML += `<div> <p> ${elem.title} by ${elem.author}</p> <input type= 'button' class= 'Remove' value='Remove' <hr>></div> `;
 });
 /* tracking events */
-addBtn.addEventListener('click',updateBooks);
+addBtn.addEventListener('click', updateBooks);
 /* store datas. */
 const titleBook = document.getElementById('title-book');
 const authorBook = document.getElementById('author-book');
-function setStorage(){
+function setStorage() {
   const inputBook = {
     bookTitle: document.getElementById('title-book').value,
-    bookAuthor: document.getElementById('author-book').value
+    bookAuthor: document.getElementById('author-book').value,
   };
   localStorage.setItem('BookIdentifier', JSON.stringify(inputBook));
 }
@@ -55,10 +59,9 @@ function setDesign() {
   document.getElementById('title-book').value = book.bookTitle;
   document.getElementById('author-book').value = book.bookAuthor;
 }
-if(!localStorage.getItem('BookIdentifier')){
+if (!localStorage.getItem('BookIdentifier')) {
   setStorage();
-}
-else {
+} else {
   setDesign();
 }
 titleBook.onchange = setStorage;
