e29dfa72ae59c05b0a0845670ff356934b94e7bb
Taher <ttaherhagui@gmail.com>
2021-10-19 22:16:26 +0100

    fix linter errors.

--- scripts/class.js
+++ scripts/class.js
@@ -1,73 +1,76 @@
 /* Create book class */
 class Books {
-    constructor(bookLIst) {
-      this.books = bookLIst;
-    }
-    add (bookTitle,bookAuthor) {
-      const title = bookTitle;
-      const author = bookAuthor;
-      const newBook = {
-        title,
-        author,
-      };
-      this.books.push(newBook);
-    }
-    remove (bookTitle) {
-      let index = 0;
-      for(let i=0; i<this.books.length; i += 1){
-        if (this.books[i] === bookTitle){
-          index = i ;
-          break;
-        }
-      }
-      this.books.splice(index, 1);
-    }
+  constructor(bookLIst) {
+    this.books = bookLIst;
   }
+
+  add(bookTitle, bookAuthor) {
+    const title = bookTitle;
+    const author = bookAuthor;
+    const newBook = {
+      title,
+      author,
+    };
+    this.books.push(newBook);
+  }
+
+  remove(bookTitle) {
+    let index = 0;
+    for (let i = 0; i < this.books.length; i += 1) {
+      if (this.books[i] === bookTitle) {
+        index = i;
+        break;
+      }
+    }
+    this.books.splice(index, 1);
+  }
+}
 /* create variables. */
-let targetDiv = document.querySelector('.books');
+const targetDiv = document.querySelector('.books');
 let myBooks = new Books([]);
 const addBtn = document.querySelector('.add-book');
+let removeBtns = Array.from(document.querySelectorAll('.remove'));
 /* create books object variables */
 if (localStorage.getItem('myBooks')) {
-  myBooks = new Books (JSON.parse(localStorage.getItem('myBooks')));
+  myBooks = new Books(JSON.parse(localStorage.getItem('myBooks')));
 }
 /* utilities */
 function displayBooks(bookObejct) {
   const bookArray = bookObejct.books;
-  bookArray.forEach(element => {
+  bookArray.forEach((element) => {
     targetDiv.innerHTML += `<div><p>${element.title} by ${element.author}</p> <input type='button' value='remove' class='remove'> <hr></div>`;
   });
 }
 function removeBook(e) {
-    console.log('works...');
-    const targetBtn = e.target;
-    const targetBlock = targetBtn.parentNode;
-    const bookData = Array.from(targetDiv.children)[0].textContent;
-    const bookTitle = bookData.split('by')[0];
-    myBooks.remove(bookTitle);
-    localStorage.setItem('MyBooks',JSON.stringify(myBooks.books));
-    targetBlock.innerHTML = '';
+  console.log('works...');
+  const targetBtn = e.target;
+  const targetBlock = targetBtn.parentNode;
+  const bookData = Array.from(targetDiv.children)[0].textContent;
+  const bookTitle = bookData.split('by')[0];
+  myBooks.remove(bookTitle);
+  localStorage.setItem('MyBooks', JSON.stringify(myBooks.books));
+  targetBlock.innerHTML = '';
 }
 function addNewBook() {
   const bookTitle = document.getElementById('title-book').value;
   const bookAuthor = document.getElementById('author-book').value;
   myBooks.add(bookTitle, bookAuthor);
   targetDiv.innerHTML += `<div><p>${bookTitle} by ${bookAuthor}</p> <input type='button' value='remove' class='remove'> <hr></div>`;
-  localStorage.setItem('myBooks',JSON.stringify(myBooks.books));
+  localStorage.setItem('myBooks', JSON.stringify(myBooks.books));
   removeBtns = Array.from(document.querySelectorAll('.remove'));
   removeBtns.forEach((element) => {
-    element.addEventListener('click',removeBook,false);
+    element.addEventListener('click', removeBook, false);
   });
 }
-/* Load page */ 
+/* Load page */
 targetDiv.innerHTML = '';
 displayBooks(myBooks);
-/* tracking add button */ 
-addBtn.addEventListener('click',addNewBook);
+/* tracking add button */
+addBtn.addEventListener('click', addNewBook);
 /* tracking the remove buttons */
-let removeBtns = Array.from(document.querySelectorAll('.remove'));
+removeBtns = Array.from(document.querySelectorAll('.remove'));
 removeBtns.forEach((element) => {
-  element.addEventListener('click',removeBook,false);
+  element.addEventListener('click', removeBook, false);
 });
 
 // set local storage
